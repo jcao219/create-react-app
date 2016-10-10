1655061514f9dac1eee9998738c22202228b5256
Jimmy Cao <jc2462@cornell.edu>
2016-10-10 12:37:46 -0400

    Try to get this to work.

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -123,7 +123,7 @@ module.exports = {
         // @remove-on-eject-begin
         query: {
           babelrc: false,
-          presets: [require.resolve('babel-preset-react-app')],
+          presets: [require.resolve('babel-preset-react')],
         },
         // @remove-on-eject-end
       },
